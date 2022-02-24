80dd84b6299b9f688218c1e210b8fc3bf7ec8796
Hundmat <47656465+Hundmat@users.noreply.github.com>
2022-02-24 22:48:53 +0100

    Add files via upload

--- lab4/src/lab4/gui/GamePanel.java
+++ lab4/src/lab4/gui/GamePanel.java
@@ -1,5 +1,83 @@
 package lab4.gui;
 
-public class GamePanel {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.Observable;
+import java.util.Observer;
 
+import javax.swing.JPanel;
+
+import lab4.data.GameGrid;
+import lab4.data.GameGrid.Player;
+
+/**
+ * A panel providing a graphical view of the game board
+ */
+
+public class GamePanel extends JPanel implements Observer{
+
+	private final int UNIT_SIZE = 20;
+	public GameGrid grid;
+	
+	/**
+	 * The constructor
+	 * 
+	 * @param grid The grid that is to be displayed
+	 */
+	public GamePanel(GameGrid grid){
+		this.grid = grid;
+		grid.addObserver(this);
+		Dimension d = new Dimension(grid.getSize()*UNIT_SIZE+1, grid.getSize()*UNIT_SIZE+1);
+		this.setMinimumSize(d);
+		this.setPreferredSize(d);
+		this.setBackground(Color.WHITE);
+	}
+
+	/**
+	 * Returns a grid position given pixel coordinates
+	 * of the panel
+	 * 
+	 * @param x the x coordinates
+	 * @param y the y coordinates
+	 * @return an integer array containing the [x, y] grid position
+	 */
+	public int[] getGridPosition(int x, int y){
+		return new int[] {x / UNIT_SIZE, y / UNIT_SIZE};
+	}
+	
+	public void update(Observable arg0, Object arg1) {
+		this.repaint();
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		g.setColor(Color.WHITE);
+        g.fillRect(0, 0, grid.getSize() * UNIT_SIZE, grid.getSize() * UNIT_SIZE);
+        
+        for(int y = 0; y < grid.getSize(); y++) {
+    	    for(int x = 0; x < grid.getSize(); x++) {
+    	    	switch (grid.getLocation(x, y)) {
+	    	    	case OTHER:
+	    	    		g.setColor(Color.RED);
+	    			case ME:
+	    				g.setColor(Color.GREEN);    			 
+	    			default:
+	    			   break;
+    	    	}
+    	    	g.fillRect(x * UNIT_SIZE, y * UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
+    	    }
+        }
+        
+        for(int y = 0; y < grid.getSize(); y++) {
+    	    for(int x = 0; x < grid.getSize(); x++) {
+    	    	g.setColor(Color.BLACK);
+    	    	g.drawLine(x, y, x*UNIT_SIZE, y*UNIT_SIZE);
+    	    }
+    	}
+	}
 }
+
+	
+
